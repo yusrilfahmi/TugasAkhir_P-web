4dfc8e7eb560f9a74164880571a6d7c09ea6257b
yusrilfahmi <ysrfahmi@gmail.com>
2024-07-10 15:38:42 +0700

    Update

--- src/EditProfile.jsx
+++ src/EditProfile.jsx
@@ -1,50 +1,42 @@
 import { useNavigate } from 'react-router-dom';
 import EditProfileCSS from './EditProfile.module.css'
-import logo from './assets/pp.jpg';
-import { FaUser, FaLock } from "react-icons/fa";
+import logo from './assets/gray.png';
+import { FaUser, FaLock, FaCamera } from "react-icons/fa";
 
 function EditProfile() {
     return (
-        <>
-            <div className={EditProfileCSS.closeWrapper}>
-                <ul className={EditProfileCSS.close}>
-                    <li>
-                        <p>X</p>
-                    </li>
-                    <li>
-                        <h2>Edit Profile</h2>
-                    </li>
-                </ul>
+        <div className={EditProfileCSS.wrapper}>
+            <ul className={EditProfileCSS.close}>
+                <FaCamera className={EditProfileCSS.icon} />
+                <li>
+                    <button><h2>X</h2></button>
+                </li>
+                <li><h2>Edit Profile</h2></li>
+            </ul>
+            <img src={logo} alt="Logo" className={EditProfileCSS.img} />
+            <h3>Nama</h3>
+            <div className={EditProfileCSS.inputBox}>
+                <input type='nama' ></input>
+            </div>
+            <h3>Tanggal lahir</h3>
+            <div className={EditProfileCSS.inputBox}>
+                <input type='ttl'></input>
+            </div>
+            <h3>Lokasi</h3>
+            <div className={EditProfileCSS.inputBox}>
+                <input type='ttl'></input>
+            </div>
+            <h3>Pekerjaan</h3>
+            <div className={EditProfileCSS.inputBox}>
+                <input type='ttl'></input>
+            </div>
+            <h3>Hobi</h3>
+            <div className={EditProfileCSS.inputBox}>
+                <input type='ttl'></input>
             </div>
 
-            <div className={EditProfileCSS.wrapper}>
-
-                <img src={logo} alt="Logo" className={EditProfileCSS.img} />
-                <h3>Nama</h3>
-                <div className={EditProfileCSS.inputBox}>
-                    <input type='nama' ></input>
-                </div>
-                <h3>Tanggal lahir</h3>
-                <div className={EditProfileCSS.inputBox}>
-                    <input type='ttl'></input>
-                </div>
-                <h3>Lokasi</h3>
-                <div className={EditProfileCSS.inputBox}>
-                    <input type='ttl'></input>
-                </div>
-                <h3>Pekerjaan</h3>
-                <div className={EditProfileCSS.inputBox}>
-                    <input type='ttl'></input>
-                </div>
-                <h3>Hobi</h3>
-                <div className={EditProfileCSS.inputBox}>
-                    <input type='ttl'></input>
-                </div>
-
-                <button className={EditProfileCSS.btn}>Simpan</button>
-            </div>
-        </>
-
+            <button className={EditProfileCSS.btn}>Simpan</button>
+        </div>
     )
 }
 
